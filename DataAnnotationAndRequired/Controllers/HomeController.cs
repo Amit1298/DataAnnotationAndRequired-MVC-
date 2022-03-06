2395ea25c7fedfd51568c7cf39ef2b1547ac4a9b
Amit <amitkr24031995@gmail.com>
2022-03-06 08:41:30 +0530

    Adding DataAnnotationAndRequired

---
+++ DataAnnotationAndRequired/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using DataAnnotationAndRequired.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DataAnnotationAndRequired.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Employee e)
+        {
+            if(ModelState.IsValid == true)
+            {
+                ViewData["SuccessMessage"] = "<script>alert('Data has been successfully')</script>";
+                ModelState.Clear();
+            }
+            return View();
+        }
+    }
+}
---
+++ DataAnnotationAndRequired/Models/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DataAnnotationAndRequired.Models
+{
+    public class Employee
+    {
+        [Required(ErrorMessage ="Id is Mandatary")]
+        public int EmployeeId { get; set; }
+        [Required(ErrorMessage = "Name is Mandatary")]
+        [StringLength(20,MinimumLength =5)]
+        public string EmployeeName { get; set; }
+        [Required(ErrorMessage = "Age is Mandatary")]
+        public int? EmployeeAge { get; set; }
+        [Required(ErrorMessage = "Gender is Mandatary")]
+        public string EmployeeGender { get; set; }
+        [Required(ErrorMessage = "Email is Mandatary")]
+        public string EmployeeEmail { get; set; }
+
+    }
+}
